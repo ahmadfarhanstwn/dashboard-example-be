0c4b04554cce78bd5b48ad803275f36072ff8a4b
Ahmad Farhan <ahmadfarhanstwn@gmail.com>
2023-06-11 00:57:08 +0700

    get products api

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 
-// import clientRoutes from "./routes/client.js";
+import clientRoutes from "./routes/client.js";
 import generalRoutes from "./routes/general.js";
 // import managementRoutes from "./routes/management.js";
 // import salesRoutes from "./routes/sales.js";
 
 import {
-  dataUser
+  dataUser,
+  dataProduct,
+  dataProductStat,
 } from "./data/index.js";
 import User from "./models/User.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
 
 /* CONFIGURATION */
 dotenv.config();
@@ -28,7 +32,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* ROUTES */
-// app.use("/client", clientRoutes);
+app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 // app.use("/management", managementRoutes);
 // app.use("/sales", salesRoutes);
--- routes/client.js
+++ routes/client.js
@@ -1,16 +1,16 @@
-// import express from "express";
-// import {
-//   getProducts,
+import express from "express";
+import {
+  getProducts,
 //   getCustomers,
 //   getTransactions,
 //   getGeography,
-// } from "../controllers/client.js";
+} from "../controllers/client.js";
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.get("/products", getProducts);
+router.get("/products", getProducts);
 // router.get("/customers", getCustomers);
 // router.get("/transactions", getTransactions);
 // router.get("/geography", getGeography);
 
-// export default router;
+export default router;
