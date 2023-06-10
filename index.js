88ac2b0798854c78cd19148926087420d28bdab6
Ahmad Farhan <ahmadfarhanstwn@gmail.com>
2023-06-10 23:20:58 +0700

    redux toolkit rtk

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 
-import clientRoutes from "./routes/client.js";
+// import clientRoutes from "./routes/client.js";
 import generalRoutes from "./routes/general.js";
-import managementRoutes from "./routes/management.js";
-import salesRoutes from "./routes/sales.js";
+// import managementRoutes from "./routes/management.js";
+// import salesRoutes from "./routes/sales.js";
+
+import {
+  dataUser
+} from "./data/index.js";
+import User from "./models/User.js";
 
 /* CONFIGURATION */
 dotenv.config();
@@ -23,10 +28,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* ROUTES */
-app.use("/client", clientRoutes);
+// app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
-app.use("/management", managementRoutes);
-app.use("/sales", salesRoutes);
+// app.use("/management", managementRoutes);
+// app.use("/sales", salesRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
